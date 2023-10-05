7dff2e8b8bedd255b2814e1d9af1930e72f4f7bc
kamal <kamade123@gmail.com>
2023-10-05 02:30:20 +0100

    fetched some data from prismic

--- app.js
+++ app.js
@@ -7,12 +7,14 @@ const path = require('path')
 const port = 3000
 
 const Prismic = require('@prismicio/client');
-const PrismicDOM = require('prismic-dom')
+const PrismicH = require('@prismicio/helpers')
+const fetch = require('node-fetch')
 
 const initApi = (req)=>{
- return Prismic.getApi(process.env.PRISMIC_ENDPOINT,{
+ return Prismic.createClient(process.env.PRISMIC_ENDPOINT,{
   accessToken: process.env.PRISMIC_ACCESS_TOKEN,
-  req
+  req,
+  fetch
  })
 }
 
@@ -33,27 +35,95 @@ app.use((req, res, next)=>{
   linkResolver: handleLinkResolver
  }
 
- res.locals.PrismicDOM = PrismicDOM;
+ res.locals.PrismicH = PrismicH;
  next()
 })
 
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
-app.get('/', (req, res)=>{
- res.render('pages/home')
+app.locals.basedir = app.get('views')
+
+const handleRequest = async(api) =>{
+ // const [metadata, home, about] = await Promise.all([
+ //  api.getSingle('meta'),
+ //  api.getSingle('h'),
+ //  api.getSingle('about'),
+ // ])
+ const home = await api.getSingle('h')
+ // const metadata = await api.getSingle('meta')
+ const about = await api.getSingle('about')
+  const assets = [];
+
+  // home.data.gallery.forEach((item)=>[
+  //  console.log(item)
+  //  // assets.push(item.img.url)
+  // ])
+
+  about.data.gallery.forEach((item)=>[
+   assets.push(item.image.url)
+  ])
+
+  about.data.body.forEach((section)=>{
+   if(section.slice_type === 'gallery'){
+    section.items.forEach((item)=>{
+     // console.log(item.image.url)
+     assets.push(item.image.url)
+    })
+   }
+  })
+
+ return{
+  assets,
+  // metadata,
+  home,
+  // collections,
+  about
+ }
+
+
+}
+
+
+app.get('/', async(req, res)=>{
+ const api = await initApi(req)
+ const defaults = await handleRequest(api)
+
+ res.render('pages/home',{
+  ...defaults
+ })
 })
 
-app.get('/about', (req, res)=>{
- res.render('pages/about')
+app.get('/about', async(req, res)=>{
+ const api = await initApi(req)
+ const defaults = await handleRequest(api)
+ // console.log(defaults)
+  res.render('pages/about', {
+   ...defaults
+  })
 })
 
-app.get('/detail/:id', (req, res)=>{
- res.render('pages/detail')
+app.get('/detail/:id', async(req, res)=>{
+ const api = await initApi(req)
+ const defaults = await handleRequest(api)
+
+ const product = await api.getByUID('product', req.params.uid,{
+  fetchLinks:'collection.title'
+ })
+
+ res.render('pages/detail', {
+  ...defaults,
+  product
+ })
 })
 
-app.get('/collections', (req, res)=>{
- res.render('pages/collections')
+app.get('/collections', async(req, res)=>{
+ const api = await initApi(req)
+ const defaults = await handleRequest(api)
+
+ res.render('pages/collections',{
+  ...defaults
+ })
 })
 
 app.listen(port, ()=>{
