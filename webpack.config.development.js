833e91a8408aede641ac5ebcf1a5826cf3eed9d7
kamal <kamade123@gmail.com>
2023-10-21 04:30:35 +0100

    eslinnt fix and minor update

--- webpack.config.development.js
+++ webpack.config.development.js
@@ -1,19 +1,18 @@
 const path = require('path')
-const {merge} = require('webpack-merge')
-
+const { merge } = require('webpack-merge')
 
 const config = require('./webpack.config')
 
-module.exports = merge(config,{
- mode:'development',
+module.exports = merge(config, {
+  mode: 'development',
 
- devtool:'inline-source-map',
+  devtool: 'inline-source-map',
 
- devServer:{
-  
- },
+  devServer: {
 
- output:{
-  path:path.resolve(__dirname, 'public')
- }
+  },
+
+  output: {
+    path: path.resolve(__dirname, 'public')
+  }
 })
