6e2270a46c21fdc3004841adc0486bdcb5283609
kamal <kamade123@gmail.com>
2025-01-05 12:06:36 +0100

    added components

---
+++ app/components/Navigation.js
@@ -0,0 +1,53 @@
+import Component from 'classes/Component'
+import { template } from 'lodash'
+import { COLOR_BRIGHT_GREY, COLOR_QUARTER_SPANISH_WHITE } from 'utils/colors'
+import GSAP from 'gsap'
+
+export default class Navigation extends Component {
+  constructor ({ template }) {
+    super({
+      element: '.navigation',
+      elements: {
+        items: '.navigaion__list__item',
+        links: '.navigation__list__link'
+      }
+    })
+
+    this.onChange(template)
+  }
+
+  onChange () {
+    if (template === 'about') {
+      GSAP.to(this.element, {
+        color: COLOR_BRIGHT_GREY,
+        duration: 1.5
+      })
+
+      GSAP.to(this.elements.items[0], {
+        autoAlpha: 1,
+        delay: 0.75,
+        duration: 0.75
+      })
+
+      GSAP.to(this.elements.items[1], {
+        autoAlpha: 0
+      })
+    } else {
+      GSAP.to(this.element, {
+        color: COLOR_QUARTER_SPANISH_WHITE,
+        duration: 1.5
+      })
+
+      GSAP.to(this.elements.items[0], {
+        autoAlpha: 0,
+        duration: 0.75
+      })
+
+      GSAP.to(this.elements.items[1], {
+        autoAlpha: 1,
+        delay: 0.75,
+        duration: 0.75
+      })
+    }
+  }
+}
