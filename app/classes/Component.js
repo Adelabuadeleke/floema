928dd46e1bc37cb3b7de8595823eec5cd2b01dc9
kamal <kamade123@gmail.com>
2025-03-04 20:38:04 +0100

    updated main js code...😁

--- app/classes/Component.js
+++ app/classes/Component.js
@@ -26,15 +26,27 @@ export default class Component extends EventEmitter {
     this.elements = {}
 
     each(this.selectorChildren, (entry, key) => {
+      // console.log('entry', document.querySelectorAll(entry))
+      // console.log('entry --> el', document.querySelectorAll(entry) instanceof window.HTMLElement)
+      // console.log('entry --> NodeList', document.querySelectorAll(entry) instanceof window.NodeList)
+      // console.log('entry --> Array', Array.isArray(document.querySelectorAll(entry)))
+      // console.log('key', key)
       if (entry instanceof window.HTMLElement || entry instanceof window.NodeList || Array.isArray(entry)) {
+        console.log('instance', entry)
+        this.elements[key] = entry
+      } else {
+        console.log('none---->', entry)
         this.elements[key] = document.querySelectorAll(entry)
-        if (this.element[key].length === 0) {
+
+        if (this.elements[key].length === 0) {
+          console.log('=== 0')
           this.elements[key] = null
-        } else if (this.element[key].length === 1) {
+        } else if (this.elements[key].length === 1) {
+          console.log('=== 1')
           this.elements[key] = document.querySelector(entry)
+          console.log('elements', this.elements)
         }
       }
-      console.log(entry, key)
     })
   }
 
--- app/classes/Page.js
+++ app/classes/Page.js
@@ -1,195 +1,219 @@
-import GSAP from 'gsap'
-import NormalizeWheel from 'normalize-wheel'
-import each from 'lodash/each'
-import Prefix from 'prefix'
+import GSAP from "gsap";
+import NormalizeWheel from "normalize-wheel";
+import each from "lodash/each";
+import Prefix from "prefix";
 
-import Highlight from 'animations/Highlight'
-import Title from 'animations/Title'
-import Paragraph from 'animations/Paragraph'
-import Label from 'animations/Label'
-import map from 'lodash/map'
+import Highlight from "animations/Highlight";
+import Title from "animations/Title";
+import Paragraph from "animations/Paragraph";
+import Label from "animations/Label";
+import map from "lodash/map";
 
-import AsyncLoad from 'classes/AsyncLoad'
+import AsyncLoad from "classes/AsyncLoad";
 
-import { ColorsManager } from 'classes/Colors'
+import { ColorsManager } from "classes/Colors";
 
 export default class Page {
-  constructor ({
-    element,
-    elements,
-    id
-  }) {
-    this.selector = element
+  constructor({ element, elements, id }) {
+    this.selector = element;
     this.selectorChildren = {
       ...elements,
       animationsHighlights: '[data-animation="heighlight"]',
       animationsTitles: '[data-animation="title"]',
       animationsParagraph: '[data-animation="paragraph"]',
       animationsLabel: '[data-animation="label"]',
-      preloaders: '[data-src]'
-    }
-    this.id = id
+      preloaders: "[data-src]",
+    };
+    this.id = id;
 
-    this.onMouseWheelEvent = this.onMouseWheel.bind(this)
+    this.onMouseWheelEvent = this.onMouseWheel.bind(this);
 
-    this.transformPrefix = Prefix('transform')
+    this.transformPrefix = Prefix("transform");
 
     this.scroll = {
       current: 0,
       target: 0,
       last: 0,
-      limit: 0
-    }
+      limit: 0,
+    };
   }
 
-  create () {
-    this.element = document.querySelector(this.selector)
-    this.elements = {}
+  create() {
+    this.element = document.querySelector(this.selector);
+    this.elements = {};
 
     this.scroll = {
       current: 0,
       target: 0,
       last: 0,
-      limit: 0
-    }
+      limit: 0,
+    };
 
     each(this.selectorChildren, (entry, key) => {
-      if (entry instanceof window.HTMLElement || entry instanceof window.NodeList || Array.isArray(entry)) {
-        this.elements[key] = document.querySelectorAll(entry)
-        if (this.element[key].length === 0) {
-          this.elements[key] = null
-        } else if (this.element[key].length === 1) {
-          this.elements[key] = document.querySelector(entry)
+      if (
+        entry instanceof window.HTMLElement ||
+        entry instanceof window.NodeList ||
+        Array.isArray(entry)
+      ) {
+        console.log("instance - page.js", entry);
+        this.elements[key] = entry;
+      } else {
+        this.elements[key] = document.querySelectorAll(entry);
+
+        if (this.elements[key].length === 0) {
+          this.elements[key] = null;
+        } else if (this.elements[key].length === 1) {
+          this.elements[key] = document.querySelector(entry);
         }
       }
-      console.log(entry, key)
-    })
+      console.log(entry, key);
+    });
 
-    this.createAnimations()
-    this.createPreloaders()
+    this.createAnimations();
+    this.createPreloaders();
   }
 
-  createAnimations () {
-    this.animations = []
+  createAnimations() {
+    this.animations = [];
 
     // Titles
-    this.animationsTitles = map(this.elements.animationsTitles, element => {
+    this.animationsTitles = map(this.elements.animationsTitles, (element) => {
       return new Title({
-        element
-      })
-    })
+        element,
+      });
+    });
 
-    this.animations.push(...this.animationsTitles)
+    this.animations.push(...this.animationsTitles);
 
     // Highlights
-    this.animationsHighlights = map(this.elements.animationsHighlights, element => {
-      return new Highlight({
-        element
-      })
-    })
+    this.animationsHighlights = map(
+      this.elements.animationsHighlights,
+      (element) => {
+        return new Highlight({
+          element,
+        });
+      }
+    );
 
-    this.animations.push(...this.animationsTitles)
+    this.animations.push(...this.animationsTitles);
 
     // Paragraph
-    this.animationsParagraph = map(this.elements.animationsParagraph, element => {
-      return new Paragraph({
-        element
-      })
-    })
+    this.animationsParagraph = map(
+      this.elements.animationsParagraph,
+      (element) => {
+        return new Paragraph({
+          element,
+        });
+      }
+    );
 
-    this.animations.push(...this.animationsParagraph)
+    this.animations.push(...this.animationsParagraph);
 
     // Labels
-    this.animationsLabel = map(this.elements.animationsLabel, element => {
+    this.animationsLabel = map(this.elements.animationsLabel, (element) => {
       return new Label({
-        element
-      })
-    })
+        element,
+      });
+    });
 
-    this.animations.push(...this.animationsLabel)
+    this.animations.push(...this.animationsLabel);
   }
 
-  createPreloaders () {
-    this.preloaders = map(this.element.preloaders, element => {
-      return new AsyncLoad({ element })
-    })
+  createPreloaders() {
+    this.preloaders = map(this.element.preloaders, (element) => {
+      return new AsyncLoad({ element });
+    });
   }
 
-  show () {
-    return new Promise(resolve => {
+  show() {
+    return new Promise((resolve) => {
       ColorsManager.change({
-        backgroundColor: this.element.getAttribute('data-background'),
-        color: this.element.getAttribute('data-color')
-      })
-      this.animationIn = GSAP.timeline()
-      this.animationIn.fromTo(this.element, {
-        autoAlpha: 0
-      }, {
-        autoAlpha: 1
-      })
+        backgroundColor: this.element.getAttribute("data-background"),
+        color: this.element.getAttribute("data-color"),
+      });
+      this.animationIn = GSAP.timeline();
+      this.animationIn.fromTo(
+        this.element,
+        {
+          autoAlpha: 0,
+        },
+        {
+          autoAlpha: 1,
+        }
+      );
 
-      this.animationIn.call(_ => {
-        this.addEventListeners()
+      this.animationIn.call((_) => {
+        this.addEventListeners();
 
-        resolve()
-      })
-    })
+        resolve();
+      });
+    });
   }
 
-  hide () {
-    return new Promise(resolve => {
-      this.removeEventListeners(
-
-      )
-      this.animationOut = GSAP.timeline()
+  hide() {
+    return new Promise((resolve) => {
+      this.removeEventListeners();
+      this.animationOut = GSAP.timeline();
 
       this.animationOut.to(this.element, {
         autoAlpha: 0,
-        onComplete: resolve
-      })
-    })
+        onComplete: resolve,
+      });
+    });
   }
 
-  onMouseWheel (event) {
-    const { pixelY } = NormalizeWheel(event)
-    // console.log(deltaY)
+  // onMouseWheel (event) {
 
-    this.scroll.target += pixelY
-  }
+  // }
 
-  onRezize () {
+  onResize() {
     if (this.elements.wrapper) {
-      this.scroll.limit = this.elements.wrapper.clientHeight - window.innerHeight
+      this.scroll.limit =
+        this.elements.wrapper.clientHeight - window.innerHeight;
     }
 
-    each(this.animationsTitles, animation => animation.onRezize())
+    each(this.animationsTitles, (animation) => animation.onRezize());
   }
 
-  update () {
-    this.scroll.current = GSAP.utils.clamp(0, this.scroll.limit, this.scroll.target)
-    this.scroll.current = GSAP.utils.interpolate(this.scroll.current, this.scroll.target, 0.1)
+  onwheel({ pixelY }) {
+    this.scroll.target += pixelY;
+  }
+
+  update() {
+    this.scroll.current = GSAP.utils.clamp(
+      0,
+      this.scroll.limit,
+      this.scroll.target
+    );
+    this.scroll.current = GSAP.utils.interpolate(
+      this.scroll.current,
+      this.scroll.target,
+      0.1
+    );
 
     if (this.scroll.target < 0.01) {
-      this.scroll.target = 0
+      this.scroll.target = 0;
     }
 
     if (this.elements.wrapper) {
-      this.elements.wrapper.style[this.transformPrefix] = `translateY(-${this.scroll.current}px)`
+      this.elements.wrapper.style[
+        this.transformPrefix
+      ] = `translateY(-${this.scroll.current}px)`;
     }
   }
 
-  addEventListeners () {
-    window.addEventListener('mousewheel', this.onMouseWheelEvent)
+  addEventListeners() {
+    window.addEventListener("mousewheel", this.onMouseWheelEvent);
   }
 
-  removeEventListeners () {
-    window.addEventListener('mousewheel', this.onMouseWheelEvent)
+  removeEventListeners() {
+    window.addEventListener("mousewheel", this.onMouseWheelEvent);
   }
 
   /**
    * Destroy
    */
-  destroy () {
-    this.removeEventListeners()
+  destroy() {
+    this.removeEventListeners();
   }
 }
