9eea44b60afd7a805715b34c1e95eb133b993e53
kamal <kamade123@gmail.com>
2025-03-05 03:40:34 +0100

    added canvas implementation for webgl integration

---
+++ app/components/Canvas/About/Media.js
@@ -0,0 +1,142 @@
+import { Mesh, Program, Texture } from 'ogl'
+import GSAP from 'gsap'
+
+import vertex from 'shaders/plain-vertex.glsl'
+import fragment from 'shaders/plain-fragment.glsl'
+// eslint-disable-next-line no-unused-vars
+import _ from 'lodash'
+// import GSAP from 'gsap'
+
+export default class {
+  constructor ({ element, geometry, gl, index, scene, sizes }) {
+    this.element = element
+    this.geometry = geometry
+    this.gl = gl
+    this.index = index
+    this.scene = scene
+    this.sizes = sizes
+
+    this.createTexture()
+    this.createProgram()
+    this.createMesh()
+
+    this.extra = {
+      x: 0,
+      y: 0
+    }
+  }
+
+  createTexture () {
+    this.texture = new Texture(this.gl)
+    const image = this.element.querySelector('img')
+    this.image = new window.Image()
+    this.image.crossOrigin = 'anonymous'
+    this.image.src = image.getAttribute('data-src')
+    this.image.onload = (_) => (this.texture.image = this.image)
+  }
+
+  createProgram () {
+    this.program = new Program({
+      fragment,
+      vertex,
+      uniforms: {
+        tMap: { value: this.texture }
+      }
+    })
+  }
+
+  createMesh () {
+    this.mesh = new Mesh(this.gl, {
+      geometry: this.geometry,
+      program: this.program
+    })
+
+    this.mesh.setParent(this.scene)
+
+    // this.mesh.rotation.z = GSAP.utils.random(-Math.PI * 0.03, Math.PI * 0.03)
+
+    this.position.x += this.index * this.mesh.scale.x
+  }
+
+  createBounds ({ sizes }) {
+    this.bounds = this.element.getBoundingClientRect()
+    this.updateScale(sizes)
+    this.updateX()
+    this.updateY()
+  }
+
+  /**
+   * Animations
+   */
+  show () {
+    GSAP.from(this.program.uniforms.uAlpha, {
+      value: 0
+    }, {
+      value: 1
+    })
+  }
+
+  hide () {
+    GSAP.from(this.program.uniforms.uAlpha, {
+      value: 0
+    })
+  }
+
+  /**
+   * Events
+   */
+  onResze (sizes, scroll) {
+    this.extra = 0
+    // this.extra = {
+    //   x: 0,
+    //   y: 0
+    // }
+    this.createBounds(sizes)
+    this.updateX(scroll)
+    this.updateY(0)
+  }
+
+  /**
+   * Loop
+   */
+  updateScale (sizes) {
+    this.width = this.bounds.width / window.innerWidth
+    this.height = this.bounds.height / window.innerHeight
+
+    this.mesh.scale.x = this.sizes.width * this.width
+    this.mesh.scale.y = this.sizes.height * this.height
+
+    this.mesh.position.x =
+      -this.sizes.width / 2 + this.mesh.scale.x / 2 + this.x * this.sizes.width
+    this.mesh.position.y =
+      -this.sizes.height / 2 -
+      this.mesh.scale.y / 2 -
+      this.y * this.sizes.height
+    console.log(this.height, this.width)
+  }
+
+  updateX (x = 0) {
+    this.x = (this.bounds.left + x) / window.innerWidth
+
+    this.mesh.position.x =
+      -this.sizes.width / 2 +
+      this.mesh.scale.x / 2 +
+      (this.x + x) * this.sizes.width +
+      this.extra.x
+  }
+
+  updateY (y = 0) {
+    this.y = (this.bounds.top + y) / window.innerHeight
+
+    this.mesh.position.y =
+      -this.sizes.height / 2 -
+      this.mesh.scale.y / 2 -
+      (this.y + y) * this.sizes.height
+  }
+
+  update (scroll) {
+    if (!this.bounds) return
+    this.updateX(scroll.x)
+    this.updateY(scroll.y)
+  }
+}
--- app/components/Canvas/index.js
+++ app/components/Canvas/index.js
@@ -1,154 +1,228 @@
-/* eslint-disable quotes */
-/* eslint-disable comma-dangle */
-/* eslint-disable space-before-function-paren */
-/* eslint-disable semi */
-import { Camera, Renderer, Transform } from "ogl";
+import { Camera, Renderer, Transform } from 'ogl'
 
-import Home from "./Home";
+import Home from './Home'
+import About from './About'
 
 export default class Canvas {
-  constructor() {
+  constructor ({ template }) {
+    this.template = template
     this.x = {
       start: 0,
       distance: 0,
-      end: 0,
-    };
+      end: 0
+    }
 
     this.y = {
       start: 0,
       distance: 0,
-      end: 0,
-    };
-    this.createRenderer();
-    this.createCamera();
-    this.createScene();
-    this.createHome();
+      end: 0
+    }
+    this.createRenderer()
+    this.createCamera()
+    this.createScene()
+
+    this.onResize()
+    this.onChangeEnd(this.template)
+    // this.createHome()
   }
 
-  createRenderer() {
+  createRenderer () {
     this.renderer = new Renderer({
       alpha: true,
-      antialias: true,
-    });
+      antialias: true
+    })
 
-    this.gl = this.renderer.gl;
-    document.body.appendChild(this.gl.canvas);
+    this.gl = this.renderer.gl
+    document.body.appendChild(this.gl.canvas)
   }
 
-  createCamera() {
-    this.camera = new Camera(this.gl);
-    this.camera.position.z = 5;
+  createCamera () {
+    this.camera = new Camera(this.gl)
+    this.camera.position.z = 5
   }
 
-  createScene() {
-    this.scene = new Transform();
+  createScene () {
+    this.scene = new Transform()
   }
 
-  createHome() {
+  createHome () {
     this.home = new Home({
       gl: this.gl,
       scene: this.scene,
-      sizes: this.sizes,
-    });
+      sizes: this.sizes
+    })
   }
+
+  destoyHome () {
+    if (!this.home) return
+
+    this.home.destoy()
+    this.home = null
+  }
+
+  createAbout () {
+    this.about = new About({
+      gl: this.gl,
+      scene: this.scene,
+      sizes: this.sizes
+    })
+  }
+
+  destoyAbout () {
+    if (!this.about) return
+
+    this.about.destoy()
+    this.about = null
+  }
+
   /**
    * Events
    **/
+  onChangeStart () {
+    if (this.home) {
+      this.home.hide()
+    }
+    if (this.about) {
+      this.about.hide()
+    }
+  }
 
-  onResize() {
-    this.renderer.setSize(window.innerWidth, window.innerHeight);
+  onChangeEnd (template) {
+    if (template === 'home') {
+      this.createHome()
+    } else {
+      this.destoyHome()
+    }
+
+    if (template === 'about') {
+      this.createAbout()
+    } else {
+      this.destoyAbout()
+    }
+  }
+
+  onResize () {
+    this.renderer.setSize(window.innerWidth, window.innerHeight)
 
     this.camera.perspective({
-      aspect: window.innerWidth / window.innerHeight,
-    });
+      aspect: window.innerWidth / window.innerHeight
+    })
 
-    const fov = (this.camera.fov = Math.PI / 100);
-    const height = 2 * Math.tan(fov / 2) * this.camera.position.x;
-    const width = height * this.camera.aspect;
+    const fov = (this.camera.fov = Math.PI / 100)
+    const height = 2 * Math.tan(fov / 2) * this.camera.position.x
+    const width = height * this.camera.aspect
 
     this.sizes = {
       height,
-      width,
-    };
+      width
+    }
+
+    const values = {
+      sizes: this.sizes
+    }
+
+    if (this.about) {
+      this.about.onResize(values)
+    }
 
     if (this.home) {
-      this.home.onResize({
-        sizes: this.sizes,
-      });
+      this.home.onResize(values)
     }
   }
 
-  onTouchDown(event) {
-    this.isDown = true;
+  onTouchDown (event) {
+    this.isDown = true
 
-    this.x.start = event.touches ? event.touches[0].clientX : event.clientX;
-    this.y.start = event.touches ? event.touches[0].clientY : event.clientY;
+    this.x.start = event.touches ? event.touches[0].clientX : event.clientX
+    this.y.start = event.touches ? event.touches[0].clientY : event.clientY
+    const values = {
+      x: this.x,
+      y: this.y
+    }
+
+    if (this.about) {
+      this.about.onTouchDown(values)
+    }
 
     if (this.home) {
-      this.home.onTouchDown({
-        x: this.x,
-        y: this.y,
-      });
+      this.home.onTouchDown(values)
     }
   }
 
-  onTouchMove(event) {
-    if (this.isDown) return;
+  onTouchMove (event) {
+    if (this.isDown) return
 
-    const x = event.touches ? event.touches[0].clientX : event.clientX;
-    const y = event.touches ? event.touches[0].clientY : event.clientY;
+    const x = event.touches ? event.touches[0].clientX : event.clientX
+    const y = event.touches ? event.touches[0].clientY : event.clientY
 
-    this.x.end = x;
-    this.y.end = y;
+    this.x.end = x
+    this.y.end = y
 
     // this.x.distance = this.x.start - this.x.end
     // this.y.distance = this.y.start - this.y.end
+    const values = {
+      x: this.x,
+      y: this.y
+    }
+
+    if (this.about) {
+      this.about.onTouchMove(values)
+    }
 
     if (this.home) {
-      this.home.onTouchMove({
-        x: this.x,
-        y: this.y,
-      });
+      this.home.onTouchMove(values)
     }
   }
 
-  onTouchUp(event) {
-    this.isDown = false;
+  onTouchUp (event) {
+    this.isDown = false
 
-    const x = event.touches ? event.changedTouches[0].clientX : event.clientX;
-    const y = event.touches ? event.changedTouches[0].clientY : event.clientY;
+    const x = event.touches ? event.changedTouches[0].clientX : event.clientX
+    const y = event.touches ? event.changedTouches[0].clientY : event.clientY
 
-    this.x.end = x;
-    this.y.end = y;
+    this.x.end = x
+    this.y.end = y
 
     // this.x.distance = this.x.start - this.x.end
     // this.y.distance = this.y.start - this.y.end
+    const values = {
+      x: this.x,
+      y: this.y
+    }
+    if (this.about) {
+      this.about.onTouchUp(values)
+    }
 
     if (this.home) {
-      this.home.onTouchUp({
-        x: this.x,
-        y: this.y,
-      });
+      this.home.onTouchUp(values)
     }
   }
 
-  onWheel(event) {
+  onWheel (event) {
+    if (this.about) {
+      this.about.onWheel(event)
+    }
+
     if (this.home) {
-      this.home.onWheel(event);
+      this.home.onWheel(event)
     }
   }
 
   /**
    * Loops
    **/
-  update() {
+  update () {
+    if (this.about) {
+      this.about.update()
+    }
+
     if (this.home) {
-      this.home.update();
+      this.home.update()
     }
 
     this.renderer.render({
       camera: this.camera,
-      scene: this.scene,
-    });
+      scene: this.scene
+    })
   }
 }
