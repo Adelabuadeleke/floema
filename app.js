4e3ecda7967296f4c726aba69083870a26e5bcad
kamal <kamade123@gmail.com>
2025-03-06 15:11:40 +0100

    minor update

--- app.js
+++ app.js
@@ -150,6 +150,7 @@ app.get('/about', async (req, res) => {
 app.get('/detail/:uid', async (req, res) => {
   const api = await initApi(req)
   const defaults = await handleRequest(api)
+  const home = await api.getSingle('h')
   // console.log('')
   const product = await api.getByUID('product', req.params.uid, {
     fetchLinks: 'collection.title'
@@ -157,7 +158,8 @@ app.get('/detail/:uid', async (req, res) => {
   // console.log(product.data.highlights)
   res.render('pages/detail', {
     ...defaults,
-    product
+    product,
+    home
   })
 })
 
