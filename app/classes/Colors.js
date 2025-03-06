492ace4bb73beb722e7a89d259adb32199fc94b2
kamal <kamade123@gmail.com>
2025-03-06 17:31:12 +0100

    updated colors bg

--- app/classes/Colors.js
+++ app/classes/Colors.js
@@ -6,9 +6,9 @@ class Colors {
     color
   }) {
     GSAP.to(document.documentElement, {
-      backgroundColor,
+      background: backgroundColor,
       color,
-      duration: 1.5
+      duration: 0.5
     })
   }
 }
